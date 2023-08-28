1b4dfcc22e2eeb896db41b3ec2474bd76a9157f0
Quang <quangnv.124010122043@vtc.edu.vn>
2023-08-28 11:32:02 +0700

    Add Tank Prefab

---
+++ Assets/Scripts/TankStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankStats : MonoBehaviour
+{
+    public int Health;
+    public int Damage;
+    public int Speed;
+}
