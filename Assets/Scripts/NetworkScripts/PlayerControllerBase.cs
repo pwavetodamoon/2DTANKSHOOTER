133d61e9547a3f49c2dccd6a44c6a84d6b98b610
Hồ Hữu Phước <hophuoc56@gmail.com>
2023-09-05 18:25:31 +0700

    Update logic into prefabs

--- Assets/Scripts/NetworkScripts/PlayerControllerBase.cs
+++ Assets/Scripts/NetworkScripts/PlayerControllerBase.cs
@@ -18,17 +18,10 @@ public class PlayerControllerBase : NetworkTransform
     }
     public virtual void Move(Vector2 MoveDir)
     {
-        // Tính toán góc quay từ vector di chuyển
-        float angle = Mathf.Atan2(MoveDir.x, MoveDir.y) * Mathf.Rad2Deg;
-        // Debug.Log("angle: " + angle);
-        // // Thiết lập góc quay của nhân vật
-        // transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
         transform.Rotate(0, 0, -MoveDir.x);
         transform.Translate(0, MoveDir.y * Runner.DeltaTime, 0);
-        // Thiết lập vận tốc di chuyển
-        // _rigidbody2d.velocity = MoveDir.normalized * _speed * Runner.DeltaTime;
-        // _rigidbody2d.rotation = angle;
+
     }
 
 
